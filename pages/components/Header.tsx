b15fd5935486b78b195c301f647f50ec52e005d7
Cemile Beyza Düzen <beyzaaduzen@gmail.com>
2022-11-07 08:41:50 +0300

    added logos in header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,11 +1,42 @@
 import Image from "next/image";
-import redditLogo from "/Users/cbeyzaduzen/Desktop/Projeler/reddit-clone-app/assets/images/reddit-logo.png"
+import redditLogo from "/Users/cbeyzaduzen/Desktop/Projeler/reddit-clone-app/assets/images/reddit-logo.png";
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/solid";
+import {
+  BellIcon,
+  GlobeAltIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/outline";
 
 const Header = () => {
   return (
-    <div>
-      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer" >
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm ">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image className="object-contain" src={redditLogo} alt="image" />
+      </div>
+      <div className="mx-7 flex items-center xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline ">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+
+      {/* Search */}
+      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
+      </form>
+
+      <div className="flex">
+        <SparklesIcon />
+        <GlobeAltIcon />
       </div>
     </div>
   );
